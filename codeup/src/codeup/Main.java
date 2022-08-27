d0a23949013be3649169e5304be80bd6050e55d9
Back <Back@DESKTOP-EJTFUAJ>
2022-08-27 22:48:30 +0900

    27일

---
+++ codeup/src/codeup/Main.java
@@ -0,0 +1,19 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		System.out.println(-a);
+		
+		
+		sc.close();
+
+	}
+
+}
---
+++ codeup/src/codeup/_1038.java
@@ -0,0 +1,26 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class _1038 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		long n = (long)a;
+		long m = (long)b;
+		
+		
+		System.out.println(n+m);
+		
+		
+		sc.close();
+
+	}
+	
+}
