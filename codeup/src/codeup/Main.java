86881da47b7d9090d30032a9504f160470019c6c
jm-back <backshren20@naver.com>
2022-09-29 17:46:41 +0900

    9월 29일

--- codeup/src/codeup/Main.java
+++ codeup/src/codeup/Main.java
@@ -5,26 +5,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// 여기까지! 이제 그만~
 		
 		Scanner sc = new Scanner(System.in);
 		
-		// 정수 1개를 입력받고, 1~5 계속 더하다가 입력된 정수보다 커지거나 같아지는 경우, 그때 합 출력
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		
-		int n = sc.nextInt();
-		int sum = 0;
+		System.out.println(a+b);
 		
-		for(int i=1; i<=n; i++) {
-			if(sum>=n) {
-				break;
-			}
-			sum += i;
-		}
-		
-		System.out.println(sum);
 		
 		sc.close();
-	
+
 	}
 
 }
---
+++ codeup/src/codeup/baek_1.java
@@ -0,0 +1,21 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class baek_1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		
+		
+		sc.close();
+
+	}
+
+}
